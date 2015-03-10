e171e6dba504b9292f75cdcd0e7fc64e60523108
Yoshikazu Yahata <yosh@ozmiq.com>
2015-03-10 19:01:55 +0900

    first commit.

---
+++ Assets/GUIMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI () {
+        // ボタンを表示する
+        GUI.Button( new Rect(20, 20, 100, 50), "Button" );
+    }
+}
